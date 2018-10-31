b4ecbdbc832d3cb95a5f0a84e202547c470f2e6a
Vikas Ramji Singh <singh.vika@northeastern.edu>
2018-10-30 23:34:22 -0400

    returns weather json for a given address query parameter

---
+++ server.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+
+const utils = require('./utils/utils.js');
+
+const port = process.env.PORT || 3000;
+var server = express();
+
+
+server.get('/', (req, res) => {
+    res.send(
+        {
+            'name': 'vikas',
+            'age':'25' 
+        }
+    );
+})
+
+server.get('/weather', (req, res) => {
+    const address = req.query.address;
+    if (!address)
+    {
+        res.send(`please provide address`);
+    }
+    
+    utils.getCoords(address).then((location) => {
+        return utils.getWeather(location);
+    }).then((weather) => {
+        res.send(weather);
+    }).catch((err) => {
+        console.log(err);
+        res.send({});
+    })
+})
+
+server.listen(3000, (error)=> {
+    if (error)
+    {
+        console.log(error)
+    }
+    else{
+        console.log(`server @ ${port}`);
+    }
+});
---
+++ utils/utils.js
@@ -0,0 +1,73 @@
+
+const request = require('request');
+// to run it in local, pleaseinser your keys before running
+const geocode_key = process.env.geocode_key;
+const darksky_key = process.env.darksky_key;
+
+
+const getCoords = (address) => {
+    return new Promise((resolve, reject) => {
+        if (!address) {
+            reject(Error('please provide address'));
+        }
+
+        let uri = encodeURI(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${geocode_key}`);
+        makeRequest(uri).then((response) => {
+            if (response.body.status==='OK'){
+                resolve(response.body.results[0].geometry.location);
+            }
+                
+            console.log(response.body.results.length);
+            console.log(response.body.status)
+            reject(`invalid address`);
+            
+        })
+
+
+    })
+}
+
+const makeRequest = (uri) => {
+    return new Promise((resolve, reject) => {
+        if (!uri)
+        {
+            reject(Error(`no uri provided`));
+        }
+        request({uri, json: true}
+            , (error, response, body) => {
+        
+            if (error)
+                {
+                    reject(Error(error.toString()));
+                }
+                if (response.statusCode!=200 )
+                {
+                    reject(Error(`invalid request`));
+                }
+                resolve(response);
+        })
+    })
+}
+
+
+const getWeather = (location) => {
+    return new Promise((resolve, reject) => {
+        if (!location)
+        {   
+            reject(Error(`no location information`))
+        }
+
+        let uri = encodeURI(`https://api.darksky.net/forecast/${darksky_key}/${location.lat},${location.lng}`);
+
+        makeRequest(uri).then((response) => {
+            resolve(response.body.currently);
+        })
+    })
+}
+
+module.exports = {
+    getCoords,
+    getWeather
+}
+
+
